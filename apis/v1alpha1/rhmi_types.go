8f023b726f78cd852a678c7ccaaf5d59cb5f0f18
Automatron <github.eng@feedhenry.com>
2024-04-22 11:01:14 +0100

    MGDAPI-3229 - Update 3scale bundle (0.12.1-mas)
    Image: quay.io/integreatly/delorean:3scale-index-0.12.1mas-17
    Image tag: 0.12.1-mas
    Image source: https://pkgs.devel.redhat.com/git/containers/3scale-operator-prod-operator-metadata#acbeee99632b9a63099d1aebda401c6688973f62

--- apis/v1alpha1/rhmi_types.go
+++ apis/v1alpha1/rhmi_types.go
@@ -78,7 +78,7 @@ var (
 	OperatorVersionRHSSO          OperatorVersion = "7.6.9-3"
 	OperatorVersionRHSSOUser      OperatorVersion = "7.6.9-3"
 	OperatorVersionCloudResources OperatorVersion = "1.1.4"
-	OperatorVersion3Scale         OperatorVersion = "0.11.8-mas"
+	OperatorVersion3Scale         OperatorVersion = "0.12.1-mas"
 	OperatorVersionMarin3r        OperatorVersion = "0.13.1"
 
 	// Event reasons to be used when emitting events
